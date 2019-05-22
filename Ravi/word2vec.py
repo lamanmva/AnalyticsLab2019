c0a12d7c69a0a409d54dccd3daa9ab2331dee43d
Markus Loecher <markus.loecher@gmail.com>
2019-05-22 16:54:53 +0200

    Add files via upload

--- Ravi/word2vec.py
+++ Ravi/word2vec.py
@@ -14,3 +14,10 @@ X = model[model.wv.vocab]
 
 pca = PCA(n_components=2)
 result = pca.fit_transform(X)
+
+pyplot.scatter(result[:,0], result[:,1])
+words = list(model.wv.vocab)
+
+for i, word in enumerate(words):
+    pyplot.annotate(word, xy=(result[i,0], result[i,1]))
+pyplot.show()
