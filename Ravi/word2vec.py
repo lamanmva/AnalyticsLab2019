a7fd2c49d9d00d4d9c8ef8fe202e145d62bdeecf
Markus Loecher <markus.loecher@gmail.com>
2019-05-22 16:46:43 +0200

    Add files via upload

--- Ravi/word2vec.py
+++ Ravi/word2vec.py
@@ -6,3 +6,11 @@ sentences = [["this", "is", "the", "first", "sentence", "for", "word2vec"],
             ["this", "is", "the", "second", "sentence"],
             ["one", "more", "sentence"],
             ["and", "the", "final", "sentence"]]
+			
+			
+model = Word2Vec(sentences, min_count=1)
+
+X = model[model.wv.vocab]
+
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
